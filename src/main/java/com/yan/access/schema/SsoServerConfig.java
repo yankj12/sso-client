0a72bf65febad55d2f3b0a1f88cf807c2799366c
yankj12 <yankj12@163.com>
2018-03-16 13:11:08 +0800

    将sso-server的配置配置到配置文件

---
+++ src/main/java/com/yan/access/schema/SsoServerConfig.java
@@ -0,0 +1,38 @@
+package com.yan.access.schema;
+
+public class SsoServerConfig {
+
+	/**
+	 * http/https
+	 */
+	private String protocol;
+	
+	private String ip;
+	
+	private Integer port;
+	
+	public String getProtocol() {
+		return protocol;
+	}
+
+	public void setProtocol(String protocol) {
+		this.protocol = protocol;
+	}
+
+	public String getIp() {
+		return ip;
+	}
+
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+
+	public Integer getPort() {
+		return port;
+	}
+
+	public void setPort(Integer port) {
+		this.port = port;
+	}
+	
+}
--- src/main/java/com/yan/access/service/impl/UserAccessServiceImpl.java
+++ src/main/java/com/yan/access/service/impl/UserAccessServiceImpl.java
@@ -4,18 +4,21 @@ import java.net.URI;
 
 import org.apache.http.client.utils.URIBuilder;
 
+import com.yan.access.schema.SsoServerConfig;
 import com.yan.access.service.facade.UserAccessService;
 import com.yan.access.util.HttpJsonUtil;
 import com.yan.access.vo.ResponseVo;
 
 public class UserAccessServiceImpl implements UserAccessService{
 
+	private SsoServerConfig ssoServerConfig;
+	
 	@Override
 	public ResponseVo getSession(String ticket) throws Exception{
 		URI uri = new URIBuilder()
-                .setScheme("http")
-                .setHost("localhost")
-                .setPort(8080)
+                .setScheme(ssoServerConfig.getProtocol())
+                .setHost(ssoServerConfig.getIp())
+                .setPort(ssoServerConfig.getPort())
                 .setPath("/ssoweb/UserAccessIntfServlet")
                 .setParameter("intfCode", "getSession")
                 .setParameter("ticket", ticket)
@@ -27,9 +30,9 @@ public class UserAccessServiceImpl implements UserAccessService{
 	@Override
 	public ResponseVo checkUserAuth(String userCode, String pwdhash, String ticket) throws Exception {
 		URI uri = new URIBuilder()
-                .setScheme("http")
-                .setHost("localhost")
-                .setPort(8080)
+				.setScheme(ssoServerConfig.getProtocol())
+                .setHost(ssoServerConfig.getIp())
+                .setPort(ssoServerConfig.getPort())
                 .setPath("/ssoweb/UserAccessIntfServlet")
                 .setParameter("intfCode", "checkUserAuth")
                 .setParameter("userCode", userCode)
@@ -43,9 +46,9 @@ public class UserAccessServiceImpl implements UserAccessService{
 	@Override
 	public ResponseVo invalidateSession(String ticket) throws Exception{
 		URI uri = new URIBuilder()
-                .setScheme("http")
-                .setHost("localhost")
-                .setPort(8080)
+				.setScheme(ssoServerConfig.getProtocol())
+                .setHost(ssoServerConfig.getIp())
+                .setPort(ssoServerConfig.getPort())
                 .setPath("/ssoweb/UserAccessIntfServlet")
                 .setParameter("intfCode", "invalidateSession")
                 .setParameter("userCode", "")
@@ -53,6 +56,14 @@ public class UserAccessServiceImpl implements UserAccessService{
                 .build();
 		ResponseVo responseVo = HttpJsonUtil.sendGetRequest(uri);
 		return responseVo;
+	}
+
+	public SsoServerConfig getSsoServerConfig() {
+		return ssoServerConfig;
+	}
+
+	public void setSsoServerConfig(SsoServerConfig ssoServerConfig) {
+		this.ssoServerConfig = ssoServerConfig;
 	}
 
 }
